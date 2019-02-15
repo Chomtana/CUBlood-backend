9df58af36e7c62f0126367eadfdc259a1e655c91
root <root@vultr.guest>
2019-02-15 15:38:27 +0000

    back to port 3000

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var app = module.exports = loopback();
 
 app.start = function() {
   // start the web server
-  return app.listen(3123,function() {
+  return app.listen(function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
